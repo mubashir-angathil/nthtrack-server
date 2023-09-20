d69d95f4e7c6b5ee16a11c5a51c7891d3de25937
Mubashir Angathil <mubashirangathil5142@gmail.com>
2023-09-21 00:38:32 +0530

    model creation for user

---
+++ src/models/user.models.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+
+    const User = sequelize.define('User', {
+        // Model attributes are defined here
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            unique: true,
+            validate: {
+                isEmail: true,
+                allowNull: false,
+                unique: true,
+            }
+        },
+        password: {
+            type: DataTypes.STRING(32)
+        }
+    }, {
+        // Other model options go here
+        tableName: 'users'
+    });
+
+    return User
+
+}
