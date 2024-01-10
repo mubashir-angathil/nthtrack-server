abc22709cf6b4d7d9f22cf3f916ea8c1ae387582
Mubashir Angathil <mubashirangathil5142@gmail.com>
2024-01-10 19:24:45 +0530

    Feature(API): Create getProfileDetails API

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const authRoutes = require("./routes/auth.route");
 const projectRoutes = require("./routes/project.route");
 const dataRoutes = require("./routes/data.route");
+const indexRoutes = require("./routes/index.route");
 const { errorHandler } = require("./middlewares/errorHandler.middleware");
 const scheduledTasks = require("./schedules");
 
@@ -14,6 +15,9 @@ app.use(express.json());
 
 // Enable CORS for all routes
 app.use(cors());
+
+// Mount common routes under /
+app.use("/", indexRoutes);
 
 // Mount authentication routes under /auth
 app.use("/auth", authRoutes);
